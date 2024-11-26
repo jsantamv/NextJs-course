09c7d87c0a5fe8a8ece10f4b561e6511389dc35f
Juan Carlos Santamaria V <jsantamv@gmail.com>
2024-11-25 22:09:47 -0600

    update path main

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function Home() {
@@ -5,6 +6,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <span className="text-5xl">Hi World!!  </span>
+
+        <Link href="/about" className="text-2xl text-blue-500 hover:underline">
+          About Page
+        </Link>
       </main>
     </div>
   );
