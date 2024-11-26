1419c3589c26bca32df5a569dd236b36c33feb84
Juan Carlos Santamaria V <jsantamv@gmail.com>
2024-11-25 21:08:15 -0600

    update react version

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import Link from 'next/link'
+import { HomeIcon } from '@primer/octicons-react'
+
+const navItems = [
+  {
+    label: 'Home',
+    href: '/'
+  },
+  {
+    label: 'Contact',
+    href: '/contact'
+  },
+  {
+    label: 'About',
+    href: '/about'
+  },
+  {
+    label: 'Pricing',
+    href: '/pricing'
+  }
+]
+
+
 
 export const NavBar = async () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
 
-      <Link href={'/'}>
+      <Link href={'/'} className='flex items-center'>
+        <HomeIcon className='mr-2' />
         <span>Home</span>
       </Link>
-        
-        <div className='flex flex-1'></div>
 
-        <Link className="mr-2" href="/contact">Contact</Link>
-        <Link className="mr-2" href="/about">About</Link>
-        <Link className="mr-2" href="/pricing">Princing</Link>
-        <Link className="mr-2" href="/hola">Hola</Link>
+      <div className='flex flex-1'></div>
+
+      {
+        navItems.map(navItem => {
+          return (
+            <Link key={navItem.label} className="mr-2" href={navItem.href}>{navItem.label}</Link>
+          )
+        })
+      }
     </nav>
   )
 }
